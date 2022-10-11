bd542c1dbff4009bd79a46037e04bb9f6b605044
hildelio <hildelio@gmail.com>
2022-10-11 16:02:25 -0300

    atualizando cards

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,10 +1,17 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import Cards from "../components/Cards";
+import contents from "../components/Data";
 
 class Projects extends Component {
   render() {
     return (
-      <div>
-        <p> Projects</p>
+      <div className="App-projects">
+        {
+          contents.map((item) => (<Cards
+          key={ item.id }
+          info={ item }
+          />))
+        }
       </div>
     );
   }
