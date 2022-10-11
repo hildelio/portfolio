5fdbf988cc002117ab21e20485dae9f973233450
hildelio <hildelio@gmail.com>
2022-10-11 16:05:31 -0300

    atualizando links dos cards para repositórios githubs publicos

--- src/components/Data.js
+++ src/components/Data.js
@@ -8,71 +8,77 @@ import shoppingCart from "../assets/shoppingCart.gif";
 import solarsystem from "../assets/solarsystem.jpg"
 import tryunfo from "../assets/tryunfo.png"
 import trybetunes from "../assets/trybetunes.png"
-
-
 const contents = [
   {
     id: 1,
     title: "Project Lessons Learned",
     image: lessonslearned,
     description: "O objetivo deste projeto era desenvolver um site com uma série de informações sobre o que foi aprendido na Trybe ao longo dos primeiros três blocos. O seu site deve estar com elementos posicionados e estilizados e, além disso, deve conter semântica apropriada para que seja acessível e melhor ranqueado.",
-    link: "https://github.com/tryber/sd-024-b-project-lessons-learned/pull/12"
+    link: "https://github.com/hildelio/lessonsLearned",
   },
   {
     id: 2,
     title: "Project Playground Functions",
     image: playgroundfunctions,
     description: "Neste projeto fiquei responsável por implementar o código de funções que resolvem problemas com respostas pré-determinadas, podendo utilizar a lógica de programação para ajudar na análise de cada problema e resposta esperada, facilitando a implementação do código de cada uma das funções.",
+    link: "https://github.com/hildelio/playgroundFunctions",
   },
   {
     id: 3,
     title: "Project Pixel Art",
     image: pixelart,
     description: "Para este projeto devia-se implementar um editor de arte com pixels em que a pessoa usuária poderá escolher uma cor em uma paleta de cores e poder pintar o que quiser em um quadro branco.",
+    link: "https://github.com/hildelio/pixelArt",
   },
   {
     id: 4,
     title: "Project TrybeWarts",
     image: trybewarts,
     description: "O objetivo deste projeot foi desenvolver uma página de formulário da Escola de Magia de Trybewarts, em que as pessoas estudantes poderão enviar seus feedbacks sobre ela. O tema desse projeto é baseado na obra 'Harry Potter', de J. K. Rowling, já que programar é o mais próximo que podemos chegar de algo verdadeiramente mágico!",
+    link: "https://github.com/hildelio/trybewarts",
   },
   {
     id: 5,
     title: "Project Unit Tests",
     image: unittests,
     description: "Projeto para implementar várias funções na resolução dos requisitos propostos e/ou testes unitários para garantir que as implementações das funções estão corretas, de acordo com o que está sendo solicitado em cada enunciado. Nesse projeto, fui capaz de: Escrever testes unitários utilizando o módulo Jest do NodeJS para verificar o correto funcionamento das funções; Escrever funções de forma que elas atendam a testes já implementados; Escrever funções de forma que elas atendam a testes já implementados;",
+    link: "https://github.com/hildelio/unitTests",
   },
   {
     id: 6,
     title: "Project Zoo Functions",
     image: zoofunctions,
     description: "Sabendo das suas habilidades com ES6, Higher Order Functions e testes, a prefeitura da cidade te deu uma importante missão: organizar as informações do zoológico! Você vai ser responsável por desenvolver funções que buscam informações sobre os animais do zoológico como: espécie e local de origem. Além disso, você também vai buscar dados sobre as pessoas que colaboram com a manutenção e cuidado do zoológico. E não para por aí! Você já aprendeu sobre a importância de ter uma mentalidade orientada a testes, não é mesmo? E também já sabe como a implementação de testes contribui para a escrita de códigos mais confiáveis e com boa performance. A sequência dos requisitos desse projeto foi disposta de forma a te proporcionar a experiência de entender na prática como o desenvolvimento orientado a testes ajuda a garantir um código de qualidade. Para isso, você vai implementar testes para funções já existentes, percebendo os casos de uso da sua aplicação e garantindo que ela está funcionando da maneira correta! rocket.",
+    link: "https://github.com/hildelio/zooFunctions",
   },
   {
     id: 7,
     title: "Project Shopping Cart",
     image: shoppingCart,
     description: "O objetivo foi desenvolver carrinho de compras totalmente dinâmico! Para isso, consumir dados diretamente da API do Mercado Livre para buscar produtos à venda. Após aprender sobre a importância de ter uma mentalidade orientada a testes, e também já saber como a implementação de testes contribui para a escrita de códigos mais confiáveis e com boa performance. Nesse projeto traz a experiência de pôr em prática o desenvolvimento orientado a testes, o famoso TDD (Test Driven Development)! Que ajuda a garantir um código de qualidade, percebendo os casos de uso da sua aplicação e garantindo que ela está funcionando da maneira correta!",
+    link: "https://github.com/hildelio/shoppingCart",
   },
   {
     id: 8,
     title: "Project Solar System",
     image: solarsystem,
     description: "Neste projeto, foi verificada a capacidade de: Utilizar JSX no React; Utilizar corretamente o método render() para renderizar seus componentes; Utilizar import para trazer componentes em diferentes arquivos; Criar componentes de classe em React; Criar múltiplos componentes a partir de um array; Fazer uso de props corretamente; Fazer uso de PropTypes para validar as props de um componente",
+    link: "https://github.com/hildelio/solarSystem",
   },
   {
     id: 9,
     title: "Project Tryunfo",
     image: tryunfo,
     description: "Neste projeto foi desenvolvido um jogo no estilo Super Trunfo! Ao utilizar essa aplicação uma pessoa usuária deverá ser capaz de: Criar um baralho com o tema livre; Adicionar e remover uma carta do baralho; Visualizar todas as cartas que foram adicionadas ao baralho; Jogar com o baralho criado.",
+    link: "https://github.com/hildelio/tryunfo",
   },
   {
     id: 10,
     title: "Project TrybeTunes",
     image: trybetunes,
     description: "O objetivo deste projeto era o de criar o  TrybeTunes, uma aplicação capaz de reproduzir músicas das mais variadas bandas e artistas, criar uma lista de músicas favoritas e editar o perfil da pessoa usuária logada. Essa aplicação capaz de: Fazer login; Pesquisar por uma banda ou artista; Listar os álbuns disponíveis dessa banda ou artista; Visualizar as músicas de um álbum selecionado; Reproduzir uma prévia das músicas deste álbum; Favoritar e desfavoritar músicas; Ver a lista de músicas favoritas; Ver o perfil da pessoa logada; Editar o perfil da pessoa logada;",
+    link: "https://github.com/hildelio/trybetunes",
   },
 
 ]
-
 export default contents;
