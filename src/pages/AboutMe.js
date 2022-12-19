45961901a77b32bf2dbfa42432c0b2c09dfddc9d
hildelio <hildelio@gmail.com>
2022-12-19 17:29:24 -0300

    atualizando aboutMe

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -1,59 +1,28 @@
 import React, { Component } from "react";
+import aboutMe from '../assets/aboutMe.png';
 
 class AboutMe extends Component {
   render() {
     return (
       <div className="App-aboutMe">
-        <div className="topo">
-          <div className="foto">
+          <div className="picture">
             <img
               className="avatar"
               src="https://avatars.githubusercontent.com/u/106602737?v=4"
               alt="avatar"
             />
+            <p>Connect with me:{' '}
+              <a href="https://linkedin.com/in/hildelio-junior" target="blank">
+                <img
+                  src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/linked-in-alt.svg"
+                  alt="hildelio"
+                  height="20"
+                  width="20"
+                />
+              </a>
+            </p>
           </div>
-          <div className="TituloSub">
-            <h1>Hildélio Júnior</h1>
-
-            <h2>A Web Developer from Brazil</h2>
-          </div>
-        </div>
-
-        <ul>
-          <li>🏡 Salvador - Brazil</li>
-
-          <li>
-            📚 I'm studying analysis and systems development at {' '} 
-            <a href="https://estacio.br/">Universidade Estácio de Sá</a>
-            </li>
-
-          <li>
-            📚 I'm studying web development at{' '}
-            <a href="https://www.betrybe.com/">Trybe</a>
-          </li>
-
-          <li>
-            🌱 I'm learning React 
-          </li>
-
-          <li>
-            📫 How to reach me{' '}
-            <a href="mailto:hildelio@gmail.com">hildelio@gmail.com</a>
-            <ul>
-            <li>Connect with me:{' '}
-          <a href="https://linkedin.com/in/hildelio" target="blank">
-            <img
-              src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/linked-in-alt.svg"
-              alt="hildelio"
-              height="20"
-              width="20"
-            />
-          </a>
-          </li>
-            </ul>
-
-          </li>
-        </ul>
+        <img className="object-img" src={aboutMe} alt='aboutMe' />
       </div>
     );
   }
